e3c1e24dc2d1ec883c0cf6517e3422a1f449570d
unknown <LiuJaNing@163.com>
2022-04-16 19:11:14 +0800

    前后端交互测试

--- front/logistics-project/src/router/index.js
+++ front/logistics-project/src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import test from "@/views/test";
+
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:"/",
+    name:"test",
+    component:test
+  }
 ]
 
 const router = new VueRouter({
---
+++ front/logistics-project/src/views/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <span>{{test}}</span>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "test",
+  data() {
+    return {
+      test: ''
+    }
+  },
+  methods:{
+    testMethods(){
+      this.$axios.get("http://localhost:8081/logisticsProject/test.do")
+          .then(resp=>{
+            this.test = resp.data;
+          })
+    }
+  },
+  created() {
+    this.testMethods()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
