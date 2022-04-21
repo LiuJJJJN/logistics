4ad139008ae1a3a60671496de24e5853ca56d9f7
unknown <LiuJaNing@163.com>
2022-04-21 15:56:42 +0800

    七天免登录实现

--- front/logistics-project/src/main.js
+++ front/logistics-project/src/main.js
@@ -19,37 +19,32 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-
 /**
- * 每次路由前都验证state中的timeStamp时间戳是否过期
- * 如果没过期: 再次设置为 7 天后, next 放行
- * 如果过期了: 强制跳转到 /login , 删除 localStorage 中的相关信息
+ * 每次路由前都验证 localStorage 是否过期
  */
-router.beforeEach((to, from, next)=>{
-  var TimeStamp = store.getters.getTimeStamp;
-  alert(to.fullPath);
-  //如果访问的是 登录 / 注册 页面则 放行
-  if(to.fullPath == '/login' || to.fullPath == '/register'){
+router.beforeEach((to, from, next)=> {
+  var rememberMe = store.getters.getRememberMe;
+  var timeStamp = store.getters.getTimeStamp;
+  var nowTimeStamp = new Date().getTime();
+  if (to.fullPath === '/login' || to.fullPath === '/register') { //如果访问的是 登录 / 注册 页面则 放行
     next();
-  } else if (TimeStamp) {
-    var nowTimeStamp = new Date().getTime();
-    //判断是否已经过期
-    if (nowTimeStamp > TimeStamp){
-      //清除 localStorage 中所有内容
-      localStorage.clear();
-      //提示错误
-      ElementUI.Message.error('免登录时间已过, 请重新登录');
-      //跳转至登录页面
-      next("/login");
-    } else {
-      // 时间戳未过期 放行
-      next();
-    }
-  } else {
-    //清除 localStorage 中所有内容
-    localStorage.clear();
-    //提示错误
-    ElementUI.Message.error('您已退出登录, 请重新登录');
+  } else if (rememberMe == null) { // localStorage 为空
+    ElementUI.Message.error('您已退出登录, 请重新登录', {duration:3*1000});
     next("/login");
+  } else {
+    var addTime = 0;
+    if (rememberMe) { //勾选了 7 天免登录
+      addTime = 1000 * 60 * 60 * 24 * 7;
+    } else { //未勾选 7 天免登录时
+      addTime = 1000 * 60 * 30;
+    }
+    if (nowTimeStamp < timeStamp) { // 未过期
+      store.commit("SET_TIME_STAMP", nowTimeStamp + addTime);
+      next();
+    } else { //已过期
+      store.commit("REMOVE_INFO");
+      ElementUI.Message.error('免登录时间已过或长时间未操作, 请重新登录', {duration:3*1000});
+      next("/login");
+    }
   }
-})
+});
