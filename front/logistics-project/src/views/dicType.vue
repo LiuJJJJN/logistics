01a1cc8282ecb61d3e870eb5a705bc0eafc0a19b
litteHan <zrhnzlh@163.com>
2022-04-25 22:16:20 +0800

    数据字典前端2/5...

--- front/logistics-project/src/views/dicType.vue
+++ front/logistics-project/src/views/dicType.vue
@@ -39,19 +39,19 @@
           prop="zip"
           label="操作"
           width="85">
-        <template slot-scope="scope">
+        <template slot-scope="">
           <el-button
               size="mini"
-              @click="showDialog(scope.$index, scope.row)" >修改</el-button>
+              @click="dialogFormVisible = true" >修 改</el-button>
         </template>
       </el-table-column><el-table-column
         prop="zip"
         label=""
         width="100">
-      <template slot-scope="scope">
+      <template slot-scope="">
         <el-button
             size="mini"
-            @click="showDialog(scope.$index, scope.row)" >删除</el-button>
+            @click="dialogFormVisible = true" >删 除</el-button>
       </template>
     </el-table-column>
     </el-table>
@@ -68,7 +68,31 @@
       </el-pagination>
     </div>
     <!--  选择权限模态窗口-->
+<!--    <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>-->
 
+    <el-dialog title="修改" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="代码" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+<!--        <el-form-item label="活动区域" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>-->
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
 
   </dev>
 </template>
@@ -127,8 +151,43 @@ export default {
         city: '普陀区',
         address: '上海市普陀区金沙江路 1518 弄',
         zip: 200333
-      }]
+      }],
+      gridData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      //模态窗口
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formLabelWidth: '120px'
+
     }
+  },
+  methods:{
+
   }
 }
 </script>
