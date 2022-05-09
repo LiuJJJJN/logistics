77658e1d824f0098db09cc894a519d3de72b8890
ning <LiuJaNing@163.com>
2022-05-09 14:41:45 +0800

    服务器更换，相关配置更改

--- front/logistics-project/src/axios.js
+++ front/logistics-project/src/axios.js
@@ -4,9 +4,8 @@ import router from './router';
 import ElementUI from 'element-ui';
 
 //配置 axios 每个链接的公共字符
-// axios.defaults.baseURL = "http://47.111.84.87:8080/logisticsProject";
-axios.defaults.baseURL = "http://47.113.216.124:8080/logisticsProject";
-// axios.defaults.baseURL = "http://127.0.0.1:8080/logisticsProject";
+// axios.defaults.baseURL = "http://47.113.216.124:8080/logisticsProject";
+axios.defaults.baseURL = "http://127.0.0.1:8080/logisticsProject";
 
 //配置 axios 可以带 cookie 存储到浏览器
 axios.defaults.withCredentials = true;
